c86d93799c6f3357d9026f0214470905425b2a91
Arka <arkaprava.123040@gmail.com>
2022-10-24 21:45:19 +0530

    Add project files.

---
+++ Kafka.Producer/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Confluent.Kafka;
+
+Console.WriteLine("Kafka Producer");
+Console.WriteLine("Press ctrl+c to cancel");
+var conf = new ProducerConfig { BootstrapServers = "kafka.confluent.svc.cluster.local:9092" };
+
+Action<DeliveryReport<Null, string>> handler = r =>
+    Console.WriteLine(!r.Error.IsError
+        ? $"Delivered message to {r.TopicPartitionOffset}"
+        : $"Delivery Error: {r.Error.Reason}");
+CancellationTokenSource cts = new CancellationTokenSource();
+Console.CancelKeyPress += (_, e) => {
+    e.Cancel = true; // prevent the process from terminating.
+    cts.Cancel();
+};
+
+try
+{
+    using (var p = new ProducerBuilder<Null, string>(conf).Build())
+    {
+        while (!cts.IsCancellationRequested)
+        {
+            p.Produce("test101", new Message<Null, string> { Value = Guid.NewGuid().ToString() }, handler);
+        }
+
+        // wait for up to 10 seconds for any inflight messages to be delivered.
+        p.Flush(TimeSpan.FromSeconds(10));
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception Occured: {ex}");
+}
